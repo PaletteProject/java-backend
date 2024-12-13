9f192705a3a65f6dc9f9b456aac67af1987b3910
jake <jsmith181997@gmail.com>
2024-12-13 12:44:05 -0600

    chore: serve react frontend with spring

---
+++ src/main/java/palette/JavaPaletteApplication.java
@@ -0,0 +1,13 @@
+package palette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPaletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPaletteApplication.class, args);
+    }
+
+}
